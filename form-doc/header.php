5dc3218a4b9b80677d2ed11cf3203f44ab5e7d9f
Franky <franky.avila@opendat.cl>
2025-07-26 11:57:49 -0400

    correccion ruta

--- form-doc/header.php
+++ form-doc/header.php
@@ -1,5 +1,5 @@
 <?php
-define('RUTA', '/html-public/doctoradoIntranet/');
+define('RUTA', '/public_html/doctoradoIntranet/');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 if (strlen(session_id()) < 1) {
