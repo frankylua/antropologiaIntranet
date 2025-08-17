917b1231039c97b805f7bc41b8f2cac42e50ea09
Franky <franky.avila@opendat.cl>
2025-08-17 08:30:49 -0400

    prueba cambio metodo ajax

--- js/buttons.js
+++ js/buttons.js
@@ -3,44 +3,44 @@ $(document).ready(function(){
         location.href='index.php'
     })
     $('#btn_acad_alum').click(function(){
-        location.href=ruta+'estudiante/ingr.dat.prog.php'
+        location.href+'../estudiante/ingr.dat.prog.php'
     })
     $('#btn_acad_prof').click(function(){
-        location.href=ruta+'docente/ingr.dat.prog.php'
+        location.href='../docente/ingr.dat.prog.php'
     })
     $('.login').click(function(){
         location.href='index.php'
     })
     $('.inicio_admin').click(function(){
-        location.href=ruta+'admin/inicio.php'
+        location.href='../admin/inicio.php'
     })
     $('.prog_prof_volver').click(function(){
-        location.href=ruta+'docente/ingr.dat.acad.php'
+        location.href='../docente/ingr.dat.acad.php'
     })
     $('.prog_alum_volver').click(function(){
-        location.href=ruta+'estudiante/ingr.dat.acad.php'
+        location.href='../estudiante/ingr.dat.acad.php'
     })
     $('#btn_perfil').click(function(){
-        location.href='perfil.php'
+        location.href='../perfil.php'
     })
     $('.btn_edit_admin').click(function(){
         location.href='editar.php'
     })
     $('#editar_pass').click(function(){
-        location.href=ruta+'ed.pass.php'
+        location.href='../ed.pass.php'
     })
     $('#btn_acad_prof').click(function(){
-        location.href=ruta+'docente/ingr.dat.prog.php'
+        location.href='../docente/ingr.dat.prog.php'
     })
     $('.inicio').click(function(){
-        location.href=ruta+'index.php'
+        location.href='../index.php'
     })
     $('#form_ingreso').submit(function(){
-        location.href=ruta+'form-doc/ingr.dat.pers.php'
+        location.href='../form-doc/ingr.dat.pers.php'
         
     })
     $('#form_tipo').submit(function(){
-        location.href=ruta+'form-doc/ingr.dat.pers.php'
+        location.href='../form-doc/ingr.dat.pers.php'
         
     })
     
