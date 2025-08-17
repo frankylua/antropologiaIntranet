1d03164309e0f9bda215d563e9f2414ffc0e8e17
Franky <franky.avila@opendat.cl>
2025-08-17 09:24:58 -0400

    coneccion

--- config/global.php
+++ config/global.php
@@ -1,12 +1,26 @@
 <?php
+// //ip servidor
+// define('DB_HOST','localhost');
+// //nombre db
+// define('DB_NAME','c1441353_antr_db');
+// //user
+// define('DB_USERNAME','root');
+// //db pass
+// define('DB_PASS','');
+// //codificacion caracteres
+// define('DB_ENCODE','utf8');
+// //nombre del proyecto
+// define('NOM_PRO','Doctorado Antropología');
+// //ruta proyecto
+
 //ip servidor
 define('DB_HOST','localhost');
 //nombre db
 define('DB_NAME','c1441353_antr_db');
 //user
-define('DB_USERNAME','root');
+define('DB_USERNAME','c1441353_antr_db');
 //db pass
-define('DB_PASS','');
+define('DB_PASS','34TIgubeki');
 //codificacion caracteres
 define('DB_ENCODE','utf8');
 //nombre del proyecto
