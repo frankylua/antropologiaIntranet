5884aa01911738f01b349680a41b2ae48daadd12
Franky <franky.avila@opendat.cl>
2025-08-17 17:16:17 -0400

    x

--- ajax/validaciones.php
+++ ajax/validaciones.php
@@ -3,7 +3,7 @@ function limpiar_datos($datos){
     $datos= trim($datos);
     $datos= stripslashes($datos);
     $datos= htmlspecialchars($datos);
-    $datos= filter_var($datos,FILTER_SANITIZE_STRING);
+    $datos= filter_var($datos,FILTER_SANITIZE_SPECIAL_CHARS);
     $datos= strtolower($datos);
     return $datos;
 }
