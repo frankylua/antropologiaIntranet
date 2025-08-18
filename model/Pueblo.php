11b2ec2703b082bd4e159d39e35060d96ebafe3b
Franky <franky.avila@opendat.cl>
2025-08-17 22:41:10 -0400

    xxx

--- model/Pueblo.php
+++ model/Pueblo.php
@@ -1,32 +1,26 @@
 <?php
-
 if (strlen(session_id()) < 1) {
     session_start();//Validamos si existe o no la sesión
 }
 require "../config/conexion.php";
-
 class Pueblo {
     public function __construct() {
     }
-
     // Método para insertar un pueblo
     public function insertar($nombre) {
         $sql = "INSERT INTO pueblo (id_pueblo, pueblo) VALUES (NULL, '$nombre')";
         return ejecutarConsulta($sql);
     }
-
     // Método para editar un pueblo
     public function editar($id, $nombre) {
         $sql = "UPDATE pueblo SET pueblo = '$nombre' WHERE id_pueblo = '$id'";
         return ejecutarConsulta($sql);
     }
-
     // Método para mostrar todos los pueblos
     public function mostrar() {
         $sql = "SELECT * FROM pueblo ORDER BY pueblo";
         return ejecutarConsultaResultados($sql); // Devuelve array de resultados
     }
-
     // Método para eliminar un pueblo
     public function eliminar($id) {
         $sql = "DELETE FROM pueblo WHERE id_pueblo = '$id'";
