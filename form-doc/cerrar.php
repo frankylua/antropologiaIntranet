c09a7d05fb2b28b612cf704e88a73bd5aac4996c
Franky <franky.avila@opendat.cl>
2026-07-26 17:38:45 -0400

    fix(auth): terminate logout session completely

--- form-doc/cerrar.php
+++ form-doc/cerrar.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $cookieParams = session_get_cookie_params();
+
+    setcookie(session_name(), '', [
+        'expires' => time() - 42000,
+        'path' => $cookieParams['path'],
+        'domain' => $cookieParams['domain'],
+        'secure' => $cookieParams['secure'],
+        'httponly' => $cookieParams['httponly'],
+        'samesite' => $cookieParams['samesite'],
+    ]);
+}
+
 session_destroy();
-$_SESSION = array();
+
 header('Location:login.php');
-?>
+exit;
